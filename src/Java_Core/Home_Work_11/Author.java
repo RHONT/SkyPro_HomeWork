3971cd712d4d30b1fe95836d8c7e0e21ee044fe1
RHONT <rg915i@yandex.ru>
2023-05-08 00:30:14 +0300

    Домашнее задание 11 ( Объекты и классы ) done

---
+++ src/Java_Core/Home_Work_11/Author.java
@@ -0,0 +1,19 @@
+package Java_Core.Home_Work_11;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
